39e4c1249e419baa12a6e26fbd727b698d1ffa29
ssd <shunya_shishido@dwango.co.jp>
2015-06-04 01:47:16 +0900

    Initial Commit

---
+++ BootCamp01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BootCamp01
+//
+//  Created by 宍戸　俊哉 on 6/4/15.
+//  Copyright (c) 2015 Shunya Shishido. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
